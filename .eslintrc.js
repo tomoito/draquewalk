1ef8767928e6030b1caa9edf16380e5c951ff383
伊藤友恒 <tomoito@itoutomohisashinoMacBook-Air.local>
2021-01-20 20:39:19 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,101 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    project: './tsconfig.eslint.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'jsx-a11y',
+    'react',
+    'react-hooks',
+  ],
+  root: true,
+  rules: {
+'lines-between-class-members': [
+    'error',
+    'always',
+    {
+      exceptAfterSingleLine: true,
+    },
+  ],
+  // should be rewritten as `['error', { allowAsStatement: true }]` in ESLint 7 or later
+  // SEE: https://github.com/typescript-eslint/typescript-eslint/issues/1184
+  'no-void': 'off',
+  'padding-line-between-statements': [
+    'error',
+    {
+      blankLine: 'always',
+      prev: '*',
+      next: 'return',
+    },
+  ],
+  // '@typescript-eslint/no-unused-vars': [
+  //   'error',
+  //   {
+  //     'vars': 'all',
+  //     'args': 'after-used',
+  //     'argsIgnorePattern': '_',
+  //     'ignoreRestSiblings': false,
+  //     'varsIgnorePattern': '_',
+  //   },
+  // ],
+  'import/extensions': [
+    'error',
+    'ignorePackages',
+    {
+      js: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+    },
+  ],
+  "no-use-before-define":"off",
+  'react/jsx-filename-extension': [
+    'error',
+    {
+      extensions: ['.jsx', '.tsx'],
+    },
+  ],
+  '@typescript-eslint/no-unsafe-assignment':"off",
+  "@typescript-eslint/no-unsafe-call":"off",
+  "@typescript-eslint/no-unsafe-member-access":"off",
+},
+overrides: [
+  {
+    'files': ['*.tsx'],
+    'rules': {
+      'react/prop-types': 'off',
+    },
+  },
+],
+settings: {
+  'import/resolver': {
+    node: {
+      paths: ['src'],
+    },
+  },
+},
+};
+
